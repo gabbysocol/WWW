3264047749f5851c1ec19bb4d30abacde35d7930
Gab <48601948+gabbysocol@users.noreply.github.com>
2019-06-07 15:57:03 +0300

    Added main

---
+++ Server/Server/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.ServiceModel;
+using System.Collections.Generic;
+using System.ServiceModel.Channels;
+using System.Threading;
+using SERVER;
+
+namespace Server
+{
+    public class SERVER         //Server
+    {
+        [ServiceContract]
+        public interface IMyService
+        {
+            [OperationContract]
+            int[] SendLevel(int level, string time);
+
+            [OperationContract]
+            string SendResult(string str, string uri, int score);
+
+            [OperationContract]
+            string SendAllResult();
+        }
+
+        public class MyService : IMyService
+        {
+            static int NumberOfUsers = 0;
+            static List<string> WonUsers = new List<string>();
+            public static Queue<string> UriUsers = new Queue<string>();
+
+            //Good
+            public int[] SendLevel(int level, string uri)
+            {
+                ++NumberOfUsers;
+                Console.WriteLine("I get level - " + level);
+                var sudoku = new SudokuGame();
+                var arr = new int[81];
+                for (var i = 0; i < 9; ++i)
+                {
+                    for (var j = 0; j < 9; ++j)
+                    {
+                        arr[i * 9 + j] = sudoku.Numbers[i, j];
+                    }
+                }
+                return arr;
+            }
+
+            public string SendResult(string str, string time, int score)
+            {
+                WonUsers.Add("User :" + str + " time " + time + "  score - " + score);
+                var resStr = String.Join("\n", WonUsers.ToArray());
+                return resStr;
+            }
+
+            public string SendAllResult()
+            {
+                WonUsers.Add("");
+                var resStr = String.Join("\n", WonUsers.ToArray());
+                return resStr;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                ServiceHost host = new ServiceHost(typeof(MyService), new Uri("http://localhost:8001/MyService"));
+                host.AddServiceEndpoint(typeof(IMyService), new BasicHttpBinding(), "");
+                host.Open();
+                Console.WriteLine("Server Ready");
+  
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
---
+++ SudokyProject/SudokyProject/SudokyProject/WorkingWithServer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Windows;
+
+namespace SudokyProject
+{
+    public class MyServiceUser
+    {
+
+        [ServiceContract]
+        public interface IMyService
+        {
+            [OperationContract]
+            int[] SendLevel(int level, string uri);
+
+            [OperationContract]
+            string SendResult(string str, string uri);
+
+            [OperationContract]
+            string SendAllResult();
+
+        }
+        public class MyService : IMyService
+        {
+
+            public int[] SendLevel(int level, string uri)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string SendResult(string str, string uri)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string SendAllResult()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class WorkingWithServer
+        {
+            private static string _uri;
+            static string Uri
+            {
+                get
+                {
+                    return _uri;
+                }
+                set
+                {
+                    _uri = "http://localhost:" + value + "/MyService";
+                }
+            }
+
+            public static ServiceHost CreateUserHost()
+            {
+                var rand = new Random();
+                var port = rand.Next(8002, 8020);
+                Uri = Convert.ToString(port);
+
+                ServiceHost host = new ServiceHost(typeof(MyService), new Uri(Uri));
+                host.AddServiceEndpoint(typeof(IMyService), new BasicHttpBinding(), "");
+                return host;
+            }
+
+            public static int[] SendLevelFromUserToServer(int level)
+            {
+                Uri tcpUri = new Uri("http://localhost:8001/MyService");
+                EndpointAddress address = new EndpointAddress(tcpUri);
+                BasicHttpBinding binding = new BasicHttpBinding();
+                binding.ReceiveTimeout = new TimeSpan(0, 10, 0);
+
+                int[] arr = null;
+
+                using (ChannelFactory<IMyService> factory = new ChannelFactory<IMyService>(binding, address))
+                {
+                    IMyService service = factory.CreateChannel();
+                    arr = service.SendLevel(level, Uri);
+                }
+                return arr;
+            }
+
+            public static string SendResultFromUserToServer(string mess, string time, string sqore)
+            {
+                Uri tcpUri = new Uri("http://localhost:8001/MyService");
+                EndpointAddress address = new EndpointAddress(tcpUri);
+                BasicHttpBinding binding = new BasicHttpBinding();
+                string strResults;
+                using (ChannelFactory<IMyService> factory = new ChannelFactory<IMyService>(binding, address))
+                {
+                    IMyService service = factory.CreateChannel();
+                    strResults = service.SendResult(mess, time);
+                }
+                return strResults;
+            }
+
+            public static string SendAllResultFromUserToServer()
+            {
+                Uri tcpUri = new Uri("http://localhost:8001/MyService");
+                EndpointAddress address = new EndpointAddress(tcpUri);
+                BasicHttpBinding binding = new BasicHttpBinding();
+                string strResults;
+                using (ChannelFactory<IMyService> factory = new ChannelFactory<IMyService>(binding, address))
+                {
+                    IMyService service = factory.CreateChannel();
+                    strResults = service.SendAllResult();
+                }
+                return strResults;
+            }
+        }
+    }
+
+}
